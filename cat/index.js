f0dd9ed257d0642d987e6eabbe53079e1bab1696
Ketanop321 <ketanop473@gmail.com>
2024-05-22 18:57:46 +0530

    no commit

---
+++ cat/index.js
@@ -0,0 +1,11 @@
+// // script.js
+// document.addEventListener('mousemove', function(e) {
+//     const cat = document.getElementById('cat');
+//     const mouseX = e.clientX;
+//     const mouseY = e.clientY;
+//     const offset = 25; // Adjust this value based on cat image size
+
+//     // Set the position of the cat element
+//     cat.style.left = (mouseX - offset) + 'px';
+//     cat.style.top = (mouseY - offset) + 'px';
+// });
